961c7e40f91b79542d6dc4cfa2d81fa1656f21db
Vladislaus-37 <vladislaus.37@gmail.com>
2026-06-18 18:50:54 +0300

    Add -a option for LS

--- run/ls.py
+++ run/ls.py
@@ -6,16 +6,23 @@ def get_me():
     script_dir = os.path.dirname(script_path)
     return script_dir
 
+if '-a' in sys.argv:
+    all_content = True
+else:
+    all_content = False
+
+
 root_dir = os.path.dirname(get_me())
 with open(os.path.join(root_dir, 'current_user_data.txt'), 'r', encoding='utf-8') as file:
     agrm = file.readlines()[0].rstrip('\n')
 try:
     for i in os.listdir(agrm):
-        if os.path.isdir(os.path.join(agrm, i)):
-            print(f'DIR   {i}')
-        elif os.path.isfile(os.path.join(agrm, i)):
-            print(f'FILE  {i}')
-        else:
-            print(f'????  {i}')
+        if i[0] != "." or all_content:
+            if os.path.isdir(os.path.join(agrm, i)):
+                print(f'DIR   {i}')
+            elif os.path.isfile(os.path.join(agrm, i)):
+                print(f'FILE  {i}')
+            else:
+                print(f'????  {i}')
 except Exception as err:
     print([False, err])
